47f54ec7a6f4319ff0f533bf451cf713f01a51dd
Fufezan Mihai <fufexan@pm.me>
2020-07-10 17:57:56 +0300

    Firebase integration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+require('dotenv').config()
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: process.env.APIKEY,
+      authDomain: 'people-counter-legendary.firebaseapp.com',
+      databaseURL: 'https://people-counter-legendary.firebaseio.com',
+      projectId: 'people-counter-legendary',
+      storageBucket: '',
+    })
+  }
 }).$mount('#app')
